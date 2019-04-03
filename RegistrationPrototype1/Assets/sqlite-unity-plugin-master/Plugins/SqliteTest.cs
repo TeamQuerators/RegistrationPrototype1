96883bf34f5bcda7cca106a8bff2c88356532fcc
Team Querators <teamquerators@gmail.com>
2019-04-03 11:47:22 +0800

    Second Commit
    Install plugin

---
+++ RegistrationPrototype1/Assets/sqlite-unity-plugin-master/Plugins/SqliteTest.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class SqliteTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Create Database
+        string connection = "URI=file:" + Application.persistentDataPath + "/My_Database";
+
+        //Open Connection
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        //Create Table
+        IDbCommand dbcmd;
+        IDataReader reader;
+
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable =
+            "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER)";
+
+        dbcmd.CommandText = q_createTable;
+        reader = dbcmd.ExecuteReader();
+
+        //Insert values in table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (1, 5)";
+        cmnd.ExecuteNonQuery();
+
+        //Read and Print all values in table
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        string query = "SELECT * FROM my_table";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+        while (reader.Read())
+        {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        //Close connection
+        dbcon.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
